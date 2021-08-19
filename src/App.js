19fe729174aaa432cb90ab77d70cfbd536753366
Haley Hicks <haleyhicks90@yahoo.com>
2021-08-19 15:28:30 -0400

    video source added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import './App.css';
 
+// ==================
+// Component Imports
+// ==================
+import Add from './components/Add'
+
 const App = () => {
 
 // ==================
@@ -15,7 +20,7 @@ const App = () => {
 
     const handleCreate = (addShow) => {
         axios
-            .post('http://localhost:8000/api/shows',
+            .post('https://blooming-thicket-84174.herokuapp.com/api/shows',
             addShow)
             .then((response) => {
                 console.log(response)
@@ -58,13 +63,16 @@ const App = () => {
                 {shows.map((show) => {
                     return (
                         <div className='show' key={show.id}>
-                            <h4>Title: {show.title}</h4>
+                            <h4>{show.title}</h4>
                             <h5>Genre: {show.genre}</h5>
                             <h5>Year: {show.year}</h5>
                             <h5>Description: {show.description}</h5>
                             <h5>Cast: {show.cast}</h5>
                             <h5>Average Rating: {show.avg_rating}</h5>
-                            <h5>Video: {show.video}</h5>
+                            <h5>Video:</h5>
+                                <video width='320' height='240' controls>
+                                    <source src={show.video} type='video/mp4' />
+                                </video>
                             <h5>Added By: {show.added_by}</h5>
                             <h5>User Ratings: {show.user_ratings}</h5>
                             <h5>User Reviews: {show.user_reviews}</h5>
