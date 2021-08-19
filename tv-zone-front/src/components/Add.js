5a2ad437cb019c92299abf7f84b8276ec2df3f44
Haley Hicks <haleyhicks90@yahoo.com>
2021-08-19 13:03:35 -0400

    test commit

--- tv-zone-front/src/components/Add.js
+++ tv-zone-front/src/components/Add.js
@@ -2,5 +2,38 @@ import React, { useState } from 'react'
 
 // CREATE component
 const Add = (props) => {
-    let emptyShow = { title: '', genre: '', year: '', description: '', }
+
+    let emptyShow = { title: '', genre: '', year: '', description: '', cast: '', avg_rating: '', video: '', added_by: '', user_ratings: '', user_reviews: ''}
+
+    const [show, setShow] = useState(emptyShow)
+
+    return (
+        <>
+
+            <br />
+            <br />
+
+            <form>
+                <label htmlFor='title'>Title: </label>
+                <input type='text' name='title' />
+
+                <br />
+
+                <label htmlFor='genre'>Genre: </label>
+                <input type='text' name='genre' />
+
+                <br />
+
+                <label htmlFor=''></label>
+                <label></label>
+                <label></label>
+                <label></label>
+                <label></label>
+                <label></label>
+                <label></label>
+                <label></label>
+
+            </form>
+        </>
+    )
 }
